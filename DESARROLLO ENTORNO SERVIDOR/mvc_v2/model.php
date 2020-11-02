e3c4e14570ad7d3480d7473ddf919f0f05c6afc4
DESKTOP-UASASM3\Coqui <balbastrex@gmail.com>
2020-11-02 16:15:55 +0100

    Añadido progreso en MVC_v2

---
+++ DESARROLLO ENTORNO SERVIDOR/mvc_v2/model.php
@@ -0,0 +1,22 @@
+<?php
+$diccionario=array(
+    'titulo_pagina'=>'Ejemplo de MVC',
+    'titulo'=>'Articulo 123',
+    'contenido'=>'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt eius aut eaque aspernatur illo modi. Ipsa omnis vero, quo iusto fuga adipisci sapiente cupiditate, sed atque hic distinctio, tempora necessitatibus.',
+    'enlace'=>'http://www.google.es',
+    'texto_enlace'=>'Google'
+);
+$diccionario2=array(
+    'titulo_pagina'=>'Foto MVC',
+    'titulo_foto'=>'foto de MVC en PHP',
+    'foto'=>'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1024px-PHP-logo.svg.png',
+    'enlace_foto'=>'https://www.php.net/'
+);
+
+$diccionario3=array(
+    'nombre_empresa'=>'GEREVA',
+    'razon_social'=>'Gestión Residual Valencia S.L.',
+    'direccion'=>'C/Bajo el puente, s/n, Valencia',
+    'telefono'=>'961 357 986'
+)
+?>
